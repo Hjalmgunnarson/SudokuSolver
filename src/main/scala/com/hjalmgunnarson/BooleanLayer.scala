b68d388e6de60ec8ce609c9b14f16266d8b58546
Hjalmgunnarson <wtvankampen@gmail.com>
2020-01-08 12:45:56 +0100

    Initial commit

---
+++ src/main/scala/com/hjalmgunnarson/BooleanLayer.scala
@@ -0,0 +1,138 @@
+package com.hjalmgunnarson
+
+import scala.language.postfixOps
+case class BooleanLayer(index: Int, cells: List[BinaryCell]) {
+  // Lines contains the horizontal lists of cells.
+  val lines: Map[Int, List[BinaryCell]] = 1 to 9 map (i => (i, cells.filter(_.y == i))) toMap
+  // Columns contains the vertical lists of cells.
+  val columns: Map[Int, List[BinaryCell]] = 1 to 9 map (i => (i, cells.filter(_.x == i))) toMap
+  // Block contains the lists of cells grouped by block
+  val blocks: Map[Int, List[BinaryCell]] = 1 to 9 map (i => (i, cells.filter(cell => getBlockId(cell.x, cell.y) == i))) toMap
+
+  // Each line is divided into three lists of three cells.
+  val lineParts: List[ListPart] = (for {
+    lineIndex <- 1 to 9
+    blockIndex <- 1 to 3
+  } yield ListPart(lineIndex, blockIndex, cells.filter(cell => blockIndex == mod(cell.x) && lineIndex == cell.y))) toList
+
+  // Each column is divided into three lists of three cells.
+  val columnParts: List[ListPart] = (for {
+    columnIndex <- 1 to 9
+    blockIndex <- 1 to 3
+  } yield ListPart(columnIndex, blockIndex, cells.filter(cell => columnIndex == cell.x && blockIndex == mod(cell.y)))) toList
+
+  // All parts of the lines are grouped by the block they're in
+  val linePartsPerBlock: List[List[ListPart]] = (for {
+    vertBlockIndex <- 1 to 3
+    horBlockIndex <- 1 to 3
+  } yield lineParts.filter(part => mod(part.index) == vertBlockIndex && part.partIndex == horBlockIndex)) toList
+
+  // All parts of the columns are grouped by the block they're in
+  val columnPartsPerBlock: List[List[ListPart]] = (for {
+    horBlockIndex <- 1 to 3
+    vertBlockIndex <- 1 to 3
+  } yield columnParts.filter(part => mod(part.index) == horBlockIndex && part.partIndex == vertBlockIndex)) toList
+
+  // Sets value in designated cell and excludes cells with the same coordinates on the other layers
+  // Excludes cells in the same line, column and block on this layer
+  def setValue(x: Int, y: Int, value: Int): Unit = {
+    cells.find(_.hasCoordinates(x, y)).foreach(_.setValue(value == index))
+    if (this.index == value) {
+      for {cell <- lines(y) if cell.x != x} cell.setValue(false) // all other cells on this line cannot hold the number
+      for {cell <- columns(x) if cell.y != y} cell.setValue(false) // all other cells in this column cannot hold the number
+      for {cell <- blocks(getBlockId(x, y)) if cell.x != x && cell.y != y} cell.setValue(false) // all other cells in this block cannot hold the number
+    }
+  }
+
+  /**
+   * Excludes cells based on the the content of a block. If the exact position of a particular number is unknown, but it
+   * can only be placed in one row or column of a block,
+   * this means the row or column in the related blocks cannot holds this number.
+   */
+  // Excludes cells based on the the content of a block. If the exact position of a particular number is unknown, but it
+  // can only be placed in one row or column of a block,
+  // this means the row or column in the related blocks cannot hold this number.
+  def excludeCellsByBlockVsLineOrColumn(): Unit = {
+    linePartsPerBlock.map(_.filter(!_.allZeroes())).foreach {
+      // this block contains two lines where the number can't be placed. It must be place on line listPart.index
+      // exclude all the other cells in the line outside this block
+      case listPart :: Nil => for {cell <- lines(listPart.index) if mod(cell.x) != listPart.partIndex} cell.setValue(false)
+      case _ => ()
+    }
+    columnPartsPerBlock.map(_.filter(!_.allZeroes())).foreach {
+      // this block contains two lines where the number can't be placed. It must be place on column listPart.index
+      // exclude all the other cells in the column outside this block
+      case listPart :: Nil => for {cell <- columns(listPart.index) if mod(cell.y) != listPart.partIndex} cell.setValue(false)
+      case _ => ()
+    }
+  }
+
+//  def excludeCellsByBlockCombination(): Unit = {
+//    val temp = lineParts.groupBy(part => part.index).values.map(list => list.filter)
+//
+//
+//
+//  }
+
+  // Join all the lines, columns and block and search
+  def findSolution(): List[ValueCell] =
+    lines.values ++ columns.values ++ blocks.values flatMap findUniqueCandidates toList
+
+  // find the lists of cells holding exactly one empty cell. This cell should contain the number
+  def findUniqueCandidates(cells: List[BinaryCell]): Option[ValueCell] =
+    cells.filter(_.value.isEmpty) match {
+      case i :: Nil if !cells.contains(Some(true)) => Some(ValueCell(i.x, i.y, this.index))
+      case _ => None
+    }
+
+  def printLayer(): Unit = {
+    var y = 1
+    while (y <= 9) {
+      var x = 1
+      if ((y - 1) % 3 == 0) println(" -------------------")
+      while (x <= 9) {
+        if ((x - 1) % 3 == 0) print(" | ")
+        print(cells.find(_.hasCoordinates(x, y)).get.value match {
+          case None => "-"
+          case Some(true) => "1"
+          case Some(false) => "0"
+        })
+        x = x + 1
+      }
+      println(" |")
+      y = y + 1
+    }
+    println(" -------------------")
+    println(this.index)
+
+  }
+
+  // Calculate block id for a cell
+  def getBlockId(x: Int, y: Int): Int = mod(x) + ((y - 1) / 3) * 3
+
+  // Helper
+  def mod(i: Int): Int = 1 + ((i - 1) / 3)
+
+}
+
+object BooleanLayer {
+  // use the cells with the same coords on all layers to see if only one of them is empty
+  // If so, it is the sole candidate for that cell
+  def findSoleCandidates(layers: List[BooleanLayer]): List[ValueCell] = {
+    val cellsByCoordinates = for {
+      x <- 1 to 9
+      y <- 1 to 9
+    } yield layers.map(layer => (layer.index, layer.cells.find(_.hasCoordinates(x, y)).get))
+
+    cellsByCoordinates.flatMap(cellsForCoordinate => cellsForCoordinate.filter(_._2.value.isEmpty) match {
+      case (value, cell) :: Nil => Some(ValueCell(cell.x, cell.y, value))
+      case _ => None
+    }) toList
+  }
+}
+
+case class ListPart(index: Int, partIndex: Int, cells: List[BinaryCell]) {
+  def allZeroes(): Boolean = cells.forall(_.value.contains(false))
+}
+
+case class Block(x: Int, y: Int, lines: List[ListPart], columns: List[ListPart])
---
+++ src/main/scala/com/hjalmgunnarson/SudokuSolver.scala
@@ -0,0 +1,185 @@
+package com.hjalmgunnarson
+
+import scala.language.postfixOps
+
+object SudokuSolver {
+  def main(args: Array[String]): Unit = new Sudoku().go()
+}
+
+class Sudoku() {
+  // Create the empty layers to store clues
+  var layers: Seq[BooleanLayer] = for {
+    index <- 1 to 9
+  } yield {
+    val cells = for {
+      x <- 1 to 9
+      y <- 1 to 9
+    } yield BinaryCell(x, y, None)
+    BooleanLayer(index, cells.toList)
+  }
+
+  def setValue(x: Int, y: Int, value: Int): Unit = layers.foreach(_.setValue(x, y, value))
+
+  def findSolutions(): List[ValueCell] = {
+    layers.foreach(_.excludeCellsByBlockVsLineOrColumn())
+    (layers.flatMap(_.findSolution()).toList ++ BooleanLayer.findSoleCandidates(layers.toList)).distinct
+  }
+
+  /**
+   * Test
+   */
+//  def fillSudoku(): Unit = {
+//
+//    // Tests exlude blockvslinecolumn
+//    setValue(4, 4, 8)
+//    setValue(5, 4, 9)
+//    setValue(6, 4, 4)
+//
+//    setValue(4, 6, 5)
+//    setValue(5, 6, 6)
+//    setValue(6, 6, 7)
+//
+//    setValue(7, 4, 8)
+//    setValue(7, 5, 9)
+//    setValue(7, 6, 4)
+//
+//    setValue(9, 4, 5)
+//    setValue(9, 5, 6)
+//    setValue(9, 6, 7)
+//  }
+
+//  def fillSudoku(): Unit = {
+//
+//    // Tests findSoleCandidates
+//    setValue(6, 1, 1)
+//    setValue(6, 3, 6)
+//    setValue(4, 4, 4)
+//    setValue(5, 5, 8)
+//    setValue(1, 6, 2)
+//    setValue(3, 6, 9)
+//    setValue(9, 6, 7)
+//    setValue(6, 8, 3)
+//  }
+
+  /**
+   * Hardest
+   */
+    def fillSudoku(): Unit = {
+      setValue(3, 1, 5)
+      setValue(4, 1, 3)
+
+      setValue(1, 2, 8)
+      setValue(8, 2, 2)
+
+      setValue(2, 3, 7)
+      setValue(5, 3, 1)
+      setValue(7, 3, 5)
+
+      setValue(1, 4, 4)
+      setValue(6, 4, 5)
+      setValue(7, 4, 3)
+
+      setValue(2, 5, 1)
+      setValue(5, 5, 7)
+      setValue(9, 5, 6)
+
+      setValue(3, 6, 3)
+      setValue(4, 6, 2)
+      setValue(8, 6, 8)
+
+      setValue(2, 7, 6)
+      setValue(4, 7, 5)
+      setValue(9, 7, 9)
+
+      setValue(3, 8, 4)
+      setValue(8, 8, 3)
+
+      setValue(6, 9, 9)
+      setValue(7, 9, 7)
+
+    }
+
+  /**
+   * Moeilijk
+   */
+//    def fillSudoku(): Unit = {
+//      setValue(3, 1, 7)
+//      setValue(5, 1, 3)
+//      setValue(8, 1, 8)
+//
+//      setValue(4, 2, 4)
+//      setValue(5, 2, 1)
+//      setValue(6, 2, 7)
+//
+//      setValue(3, 3, 2)
+//      setValue(5, 3, 6)
+//      setValue(9, 3, 4)
+//
+//      setValue(1, 4, 2)
+//      setValue(2, 4, 4)
+//      setValue(6, 4, 6)
+//
+//      setValue(2, 5, 3)
+//      setValue(3, 5, 8)
+//      setValue(6, 5, 4)
+//      setValue(9, 5, 7)
+//
+//      setValue(2, 6, 5)
+//      setValue(7, 6, 9)
+//      setValue(9, 6, 2)
+//
+//      setValue(1, 7, 6)
+//      setValue(2, 7, 2)
+//      setValue(7, 7, 5)
+//
+//      setValue(4, 8, 1)
+//      setValue(5, 8, 9)
+//
+//      setValue(1, 9, 1)
+//      setValue(7, 9, 2)
+//      setValue(9, 9, 9)
+//
+//    }
+
+  def printCandidates(): Unit = {
+    var temp = (for {
+      layer <- layers
+      cell <- layer.cells
+      if cell.value.isEmpty
+    } yield ValueCell(cell.x, cell.y, layer.index)).groupBy(cell => cell.x + (cell.y - 1) * 9)
+    for {
+      (_, values) <- temp.toSeq.sortBy(_._1)
+      value <- values
+    } println(value)
+  }
+
+
+  def go(): Unit = {
+    fillSudoku()
+    var solutions = findSolutions()
+    layers foreach (_.printLayer())
+    while (solutions.nonEmpty) {
+      println(solutions)
+      solutions.foreach(cell => setValue(cell.x, cell.y, cell.value))
+      layers.foreach(_.printLayer())
+      solutions = findSolutions()
+    }
+//    printCandidates()
+    println("Done")
+  }
+
+}
+
+case class BinaryCell(x: Int, y: Int, var value: Option[Boolean]) {
+  def setValue(v: Boolean): Unit = value = Some(v)
+  def hasCoordinates(x: Int, y: Int): Boolean = this.x == x && this.y == y
+}
+
+case class ValueCell(x: Int, y: Int, value: Int) {
+  override def toString: String = {
+    "(" + x + ", " + y + " = " + value + ")"
+  }
+}
+
+
+
